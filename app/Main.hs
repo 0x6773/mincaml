cd60274f13447cbe21c6eee5a200f30fa0715dfa
Sumit Sahrawat <therealsumit@gmail.com>
2016-10-12 19:15:26 +0530

    Add arg-parser for main

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,38 @@
 module Main where
 
-import Lib
+import           Options.Applicative
+
+data MainOpts
+  = MainOpts
+  { inline :: Maybe Int
+  , iter   :: Maybe Int
+  , files  :: [FilePath]
+  } deriving (Show)
+
+optParser :: Parser MainOpts
+optParser
+  = MainOpts
+  <$> optional (read <$> strOption optInline)
+  <*> optional (read <$> strOption optIter)
+  <*> optFiles
+  where optInline
+          = long "inline"
+          <> metavar "INLINE"
+          <> help "max. size of functions inlined"
+        optIter
+          = long "iter"
+          <> metavar "ITER"
+          <> help "max. no. of optimizations iterated"
+        optFiles
+          = some (argument str (metavar "INPUT-FILES"))
 
 main :: IO ()
-main = someFunc
+main = execParser opts >>= mainDispatcher
+  where opts = info (helper <*> optParser) desc
+        desc
+          = fullDesc
+          <> progDesc "Compile INPUT-FILES... (all .ml) to SPARC assembly (.s)"
+          <> header "mincaml - A compiler for a subset of ML"
+
+mainDispatcher :: MainOpts -> IO ()
+mainDispatcher = undefined
